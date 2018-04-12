fa290f9d248ddaffc6e59ff79adcd17b40e2d2ca
Marcel Schoch <marcel.schoch@slf.ch>
2018-04-12 13:17:02 +0200

    Drop old UI

---
+++ scratches/scratch.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from snowmicropyn.models import model_shotnoise
+from snowmicropyn.profile import Profile
+
+np.set_printoptions(precision=4, suppress=True)
+p = Profile.load('/Users/marcel/Dropbox/SMP/pnt_examples/S31M0067.pnt')
+
+print(p.samples.shape)
+
+precision = 4
+fmt = '%.{}f'.format(precision)
+header = 'distance,lambda,f0,delta,L'
+np.savetxt('shot_noise.csv', model_shotnoise(p.samples), delimiter=',', fmt=fmt, header=header, comments='')
