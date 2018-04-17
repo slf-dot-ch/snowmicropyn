86b6cd33b83fc9b79af28a4a1aa0ca10f7e2c1d0
Marcel Schoch <marcel.schoch@slf.ch>
2018-04-17 21:16:05 +0200

    Mark non official releases (missing git hash)

--- snowmicropyn/examiner/globals.py
+++ snowmicropyn/examiner/globals.py
@@ -2,4 +2,8 @@ import snowmicropyn
 
 APP_NAME = 'snowmicropyn Examiner'
 ORG_NAME = 'slf.ch'
+HASH = snowmicropyn.git_hash()
 VERSION = snowmicropyn.__version__
+if not HASH:
+    VERSION = '{} (NO OFFICIAL RELEASE)'.format(VERSION)
+
