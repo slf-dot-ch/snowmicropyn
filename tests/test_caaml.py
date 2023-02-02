7bb7f86f54ea722bf831402b8781a5654da8c0e5
Michael Reisecker <m.r@sbg.at>
2023-02-02 14:44:39 +0100

    Add schame validation unit test for CAAML output

---
+++ tests/test_caaml.py
@@ -0,0 +1,23 @@
+# Unit test for CAAML output: validation against its schema
+
+from lxml import etree
+import os
+import requests
+import snowmicropyn as smp
+
+def validate(xml_file: str, xsd_file: str):
+    xml_doc = etree.parse(xml_file)
+    schema_doc = etree.parse(xsd_file)
+    schema = etree.XMLSchema(schema_doc)
+    schema.assertValid(xml_doc)
+
+if __name__ == "__main__":
+    pro = smp.Profile.load('../examples/profiles/S37M0876.pnt')
+    testfile = "./test_caaml.caaml"
+    pro.export_caaml(testfile)
+
+    xsd_file = "./CAAMLv6_SnowProfileIACS.xsd"
+    if not os.path.exists(xsd_file):
+        res = requests.get(f"http://caaml.org/Schemas/SnowProfileIACS/v6.0.3/{xsd_file}")
+        open(xsd_file, "wb").write(res.content)
+    validate(testfile, xsd_file)
