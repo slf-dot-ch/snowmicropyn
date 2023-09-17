23a83838f3f9b380e8df35a2439ea55b6d96bea6
Michael Reisecker <m.r@sbg.at>
2023-09-17 11:43:52 +0200

    Set cutom taskbar icon
    An icon should be designed for this, but for now we just slightly modify
    the already contained SLF logo in code. At least it is more usable than
    the generic app icon.

--- snowmicropyn/pyngui/app.py
+++ snowmicropyn/pyngui/app.py
@@ -6,6 +6,7 @@ import pathlib
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtGui
 
 from snowmicropyn.pyngui.globals import *
 from snowmicropyn.pyngui.log_window import LogWindow
@@ -15,6 +16,14 @@ from snowmicropyn.pyngui.main_window import MainWindow
 def main():
     app = QApplication(sys.argv)
     app.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    #rework SLF logo so that it is somewhat usable as a taskbar icon:
+    icon_pixmap = QtGui.QPixmap(':/icons/slflogo@2x.png')
+    transparent_mask = icon_pixmap.createMaskFromColor(QtGui.QColor('transparent'), Qt.MaskOutColor)
+    icon_pixmap.fill((QtGui.QColor('white')))
+    icon_pixmap.setMask(transparent_mask)
+    app_icon = QtGui.QIcon(icon_pixmap.scaled(256, 256))
+    app.setWindowIcon(app_icon)
 
     app.setOrganizationName(ORG_NAME)
     app.setOrganizationDomain(ORG_DOMAIN)
