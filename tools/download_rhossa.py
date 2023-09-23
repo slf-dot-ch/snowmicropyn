14f371b29fbb74ead19d4465aeaea2aae05c6e67
Michael Reisecker <m.r@sbg.at>
2023-09-23 21:17:15 +0200

    Download manually marked profiles in tools
    In addition to the RHOSSA data we already fetch we additionally
    (re-)download some pnt files, but this time with manually marked layer
    information (in the ini files as markers).

--- tools/download_rhossa.py
+++ tools/download_rhossa.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-#https://tc.copernicus.org/articles/14/1829/2020/
-#https://www.envidat.ch/dataset/wfj_rhossa
+# https://tc.copernicus.org/articles/14/1829/2020/
+# https://www.envidat.ch/dataset/wfj_rhossa
+# https://gitlab.com/lwd.met/lwd-internal-projects/rhossa-smp-matched
 
 import os
 import pathlib
 import shutil
 
+### Sample data for method 'exact': prepare PRO-files with associated CAAML files.
 # Download all available manual pits plus a single SMP measurement for this location:
 url_file = "./envidatS3paths.txt"
 dl_cmd = f"wget --no-host-directories --force-directories --input-file={url_file}"
@@ -14,8 +16,7 @@ os.system(dl_cmd)
 
 location = "./envicloud/slf/sph-ct/2020_rhossa_paper_calonne_tc/data"
 target = "../data/rhossa"
-if not os.path.exists(target):
-    os.makedirs(target, exist_ok=True)
+os.makedirs(target, exist_ok=True)
 
 # Find all SMP and manual profiles, rename them according to their parent folder (the date),
 # and move them to a different location:
@@ -28,3 +29,14 @@ for pro in profiles:
 # Now the manual and SMP profiles are connected via their file names and contained within
 # a single folder. Delete wget's (empty) download directory:
 shutil.rmtree("./envicloud")
+
+### Sample data for method 'markers': download PRO-files with manually set markers.
+# Download full dataset including CAAMLs:
+os.system('git clone https://gitlab.com/lwd.met/lwd-internal-projects/rhossa-smp-matched.git')
+target = "../data/rhossa_markers"
+os.makedirs(target, exist_ok=True)
+
+# Keep only the profiles with inis (=markers):
+for file in pathlib.Path("./rhossa-smp-matched/smp_files").glob("*.*"):
+    shutil.move(file, target)
+shutil.rmtree("./rhossa-smp-matched")
